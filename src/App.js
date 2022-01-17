8119a9609d38a576e3f538dbcc64c4e949f405b6
henry04 <hromerp22@gmail.com>
2022-01-16 23:59:51 -0300

    applying css stlyes to inputs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import useFormulario from "./hooks/useFormulario";
+import Input from "./components/Input";
 import "./App.css";
 function App() {
-  const [formulario, handleChange] = useFormulario({ name: "" });
+  const [formulario, handleChange] = useFormulario({ name: "", lastName: "" });
   console.log(formulario);
   return (
     <form>
-      <input
+      <Input
+        label="Nombre"
         name="name"
-        placeholder="Nombre"
         value={formulario.name}
+        onChange={handleChange}
+      />
+      <Input
+        label="Apellido"
+        name="lastName"
+        value={formulario.lastName}
         onChange={handleChange}
       />
     </form>
