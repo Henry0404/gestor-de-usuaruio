18e6b6876289cae58a955ca4b4571896cb7d69df
henry04 <hromerp22@gmail.com>
2022-01-17 00:27:29 -0300

    introduce the inputs inside a container

--- src/App.js
+++ src/App.js
@@ -2,27 +2,32 @@ import { useState } from "react";
 import useFormulario from "./hooks/useFormulario";
 import Input from "./components/Input";
 import Card from "./components/Card";
+import Container from "./components/Container";
 import "./App.css";
 function App() {
   const [formulario, handleChange] = useFormulario({ name: "", lastName: "" });
   console.log(formulario);
   return (
-    <Card>
-      <form>
-        <Input
-          label="Nombre"
-          name="name"
-          value={formulario.name}
-          onChange={handleChange}
-        />
-        <Input
-          label="Apellido"
-          name="lastName"
-          value={formulario.lastName}
-          onChange={handleChange}
-        />
-      </form>
-    </Card>
+    <Container>
+      <Card>
+        <div style={{ padding: 20 }}>
+          <form>
+            <Input
+              label="Nombre"
+              name="name"
+              value={formulario.name}
+              onChange={handleChange}
+            />
+            <Input
+              label="Apellido"
+              name="lastName"
+              value={formulario.lastName}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+      </Card>
+    </Container>
   );
 }
 
